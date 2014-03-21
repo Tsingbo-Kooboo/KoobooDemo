7638f5c3efc5b8027dc208695b8f3cc9ac460871
Tsingbo-Kooboo <qingbo@kooboo.com>
2014-03-21 22:56:42 +0800

    Upgrade Kooboo

--- src/publish.bat
+++ src/publish.bat
@@ -3,3 +3,4 @@ copy Web\favicon.ico html\favicon.ico
 xcopy Web\Cms_Data\Sites\Kooboo\Files html\Cms_Data\Sites\Kooboo\Files /S /E /Y /H
 xcopy Web\Cms_Data\Sites\Kooboo\Themes html\Cms_Data\Sites\Kooboo\Themes /S /E /Y /H
 xcopy Web\Cms_Data\Sites\Kooboo\Scripts html\Cms_Data\Sites\Kooboo\Scripts /S /E /Y /H
+xcopy Web\Cms_Data\Contents\Blog\Media html\Cms_Data\Contents\Blog\Media /S /E /Y /H
